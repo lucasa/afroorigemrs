cfb2e54048e363c0c030f64502f2908904537221
Lucas Alberto <lucasa@gmail.com>
2025-11-05 19:32:34 -0300

    Update logo source to local path

--- components/layout/nav/header.tsx
+++ components/layout/nav/header.tsx
@@ -25,7 +25,7 @@ export const Header = () => {
                 aria-label="home"
                 className="flex items-center space-x-2">
                 <Image
-                  src="https://assets.tina.io/c83f5d58-0f9b-4fa4-835b-b5e14901a1b1/afroorigemrs-logo-peq.jpg"
+                  src="/uploads/afroorigemrs-logo-peq.jpg"
                   alt="Logo AfroOrigem RS"
                   height={50}
                   className="h-8 w-auto"
